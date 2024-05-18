574853d2c17dbeeb1f9dbef0a6429568a5399d65
Casey Glidewell <caseyglidewell@gmail.com>
2024-05-18 01:56:33 -0700

    INCOMPLETE. Added partial solution that fails upon reaching test_add_deeper.

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,47 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
     """
-    Put docstring here
+    A binary tree structure, sorted that lower numbers are placed
+    to the left, while larger numbers are placed to the right.
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        super().__init__()
 
-    def some_method(self):
+    def add(self, value):
+
+        # If there's no root at all, this is the top of the tree. create node with requested value.
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+
+        # Check if the value being added is less than that of the root's value.
+        if self.root.value >= value:
+
+            # Is there a node already to our left? if not, create a node with the requested value.
+            # Otherwise, recurse.
+            if self.root.left is None:
+                self.root.left = Node(value)
+            else:
+                self.root = self.root.left
+                self.add(value)
+
+
+        # Value is larger than our root's.
+        else:
+
+            # Is there a node already to our right? if not, create a node with the requested value.
+            # Otherwise, recurse.
+            if self.root.right is None:
+                self.root.right = Node(value)
+            else:
+                self.root = self.root.right
+                self.add(value)
+
+    def contains(self, value):
         # method body here
         pass
+
